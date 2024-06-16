dbf9bdbda94958124507a0be6d6f3514e46b2d24
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-17 00:19:12 +0530

    About Section Initialized

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-20 text-center text-4xl font-bold">
+        About
+        <span className="text-neutral-500 font-bold"> Me</span>
+      </h2>
+    </div>
+  );
+};
+
+export default About;
