3d054c7337e7419739b32581d237ace7321c1d3d
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-18 00:31:45 +0530

    Skills Section Initialized

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="mt-20 text-center text-4xl font-bold">
+        <span className="bg-gradient-to-r from-pink-300 via-slate-200 to-purple-500 bg-clip-text text-transparent">
+          Skills
+        </span>
+      </h2>
+      <div className="mt-10 text-center text-xl tracking-wide">
+        <span>
+          Here are some of my skills on which I have been working on for the
+          <br />
+          past 2 years
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
