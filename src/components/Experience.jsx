105ed1b609672fdbd7b040038e3366638764b0a9
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-19 22:24:15 +0530

    Experience Section Initialized

---
+++ src/components/Experience.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl font-bold">
+        <span className="bg-gradient-to-r from-pink-300 via-slate-200 to-purple-500 bg-clip-text text-transparent">
+          Experience
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Experience;
