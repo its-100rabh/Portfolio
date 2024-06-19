69d744e57a908d70f0f072c0277571e7c1b9173e
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-19 22:33:45 +0530

    Experience Added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { EXPERIENCES } from "../constants";
 
 const Experience = () => {
   return (
@@ -8,6 +9,24 @@ const Experience = () => {
           Experience
         </span>
       </h1>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h6 className="mb-2 font-semibold">
+                {experience.role} -{" "}
+                <span className="text-sm text-purple-100">
+                  {experience.company}
+                </span>
+              </h6>
+              <p className="mb-4 text-neutral-400">{experience.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
