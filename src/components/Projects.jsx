f0a4ca31b43fd14d32d46530250983e8ae52ac9a
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-20 23:06:24 +0530

    Project Image Section Finalized

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { PROJECTS } from "../constants";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl font-bold">
+        <span className="bg-gradient-to-r from-pink-300 via-slate-200 to-purple-500 bg-clip-text text-transparent">
+          Projects
+        </span>
+      </h1>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4 "></div>
+            <img
+              src={project.image}
+              width={150}
+              height={150}
+              alt={project.title}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
