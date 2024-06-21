04730cc700e42e40a87e68a6dabeb783345a1f61
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-21 23:10:07 +0530

    Education Section Initialized

---
+++ src/components/Education.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="mt-20 text-center text-4xl font-bold">
+        <span className="bg-gradient-to-r from-pink-300 via-slate-200 to-purple-500 bg-clip-text text-transparent">
+          Education
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Education;
