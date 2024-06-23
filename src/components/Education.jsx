3c7b1eeb604d739b9eca6c3cbb7a5dd9708b52cb
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-06-23 23:40:53 +0530

    Education Component fixxed

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ import EducationCard from '../components/Cards/EducationCard';
 
 const Education = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
+    <div className="border-b border-neutral-900 pb-20">
       <h1 className="my-20 text-center text-4xl font-bold">
         <span className="bg-gradient-to-r from-pink-300 via-slate-200 to-purple-500 bg-clip-text text-transparent">
           Education
